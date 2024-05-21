425ec54ef1d5aa2f2610774c4e94d49e9dd69460
ssoundrarajan <ssudharson@hotmail.com>
2024-05-21 21:46:02 +1000

    Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
   public static void main(String[] args) {
     //Display Hello everyone
     System.out.println("Hello Everyone");
-    System.out.println("Hello Everyone-2");
+    System.out.println("Hello Everyone");
   }
 }
